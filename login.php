e5a678d42f26cd5d1929d627f14bc3ef03c2db56
princefajilagot <113429391+princefajilagot@users.noreply.github.com>
2022-09-17 14:56:20 +0800

    Update login.php

--- login.php
+++ login.php
@@ -18,7 +18,33 @@ $error = logIn();
     <link rel="stylesheet" href="asset/css/main.css">
 </head>
 <body onload="fetchEmployee()">
-    <?php include('inc/navbar.inc.php'); ?>
+    <nav id="navbar-example2" class="navbar bg-dark px-3 mb-3">
+        <div class="container">
+            <a class="navbar-brand text-white" href="/php_ajax_crud">Employee Management System</a>
+            <ul class="nav nav-pills">
+                <?php if(!isset($_SESSION['user_id'])){ ?>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="signup.php">Signup</a>
+                    </li>
+                <?php }else { ?>
+                    <li class="nav-item dropdown text-white">
+                        <a class="nav-link dropdown-toggle text-white" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
+                            <?php echo $_SESSION['fullname']; ?>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#scrollspyHeading3">Admin Profile</a></li>
+                            <li><a class="dropdown-item" href="#scrollspyHeading4">Settings</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="/php_ajax_crud?action=logout">Logout</a></li>
+                        </ul>
+                    </li>
+                    <?php } ?>
+            </ul>
+        </div>
+    </nav>
     <div class="container mt-5">
         <div class="row">
             <div class="card col-md-4 offset-md-4">
