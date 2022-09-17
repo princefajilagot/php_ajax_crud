9412e9a7b7ede8ff39a8906b97b989702a1e884a
princefajilagot <113429391+princefajilagot@users.noreply.github.com>
2022-09-17 14:10:46 +0800

    Delete php_ajax_crud directory

--- php_ajax_crud/php/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-spl_autoload_register(function($class){
-    require_once $_SERVER['DOCUMENT_ROOT'].'/php_oop_crud/class/'.$class.'.classes.php';
-});
-
-require_once 'function.php';
-?>
